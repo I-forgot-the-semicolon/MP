e93c127ffe4e05f96ae1bd4fc0189bf379827233
Ruben <warl0ckofshadow@gmail.com>
2020-04-12 01:23:35 +0200

    Updated

---
+++ cmake-build-debug/CMakeFiles/MP.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/libs/Admin/admin.c" "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/cmake-build-debug/CMakeFiles/MP.dir/libs/Admin/admin.c.obj"
+  "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/libs/Carrier/carrier.c" "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/cmake-build-debug/CMakeFiles/MP.dir/libs/Carrier/carrier.c.obj"
+  "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/libs/Client/client.c" "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/cmake-build-debug/CMakeFiles/MP.dir/libs/Client/client.c.obj"
+  "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/libs/Provider/provider.c" "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/cmake-build-debug/CMakeFiles/MP.dir/libs/Provider/provider.c.obj"
+  "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/libs/Utils/utils.c" "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/cmake-build-debug/CMakeFiles/MP.dir/libs/Utils/utils.c.obj"
+  "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/libs/controller.c" "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/cmake-build-debug/CMakeFiles/MP.dir/libs/controller.c.obj"
+  "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/libs/fileHandler.c" "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/cmake-build-debug/CMakeFiles/MP.dir/libs/fileHandler.c.obj"
+  "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/main.c" "C:/Users/warl0/Desktop/Escritorio/Coding/C/MP/cmake-build-debug/CMakeFiles/MP.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../libs"
+  "../libs/Admin"
+  "../libs/Carrier"
+  "../libs/Client"
+  "../libs/Provider"
+  "../libs/Utils"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MP.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MP.dir/libs/Admin/admin.c.obj"
+  "CMakeFiles/MP.dir/libs/Carrier/carrier.c.obj"
+  "CMakeFiles/MP.dir/libs/Client/client.c.obj"
+  "CMakeFiles/MP.dir/libs/Provider/provider.c.obj"
+  "CMakeFiles/MP.dir/libs/Utils/utils.c.obj"
+  "CMakeFiles/MP.dir/libs/controller.c.obj"
+  "CMakeFiles/MP.dir/libs/fileHandler.c.obj"
+  "CMakeFiles/MP.dir/main.c.obj"
+  "MP.exe"
+  "MP.exe.manifest"
+  "MP.lib"
+  "MP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
